fcd809adeabc5449843b301e1b6393a9ac358e90
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-01-03 16:03:06 +0530

    LINQ syntax - Query

--- LINQdotNET/Controller/EmployeeController.cs
+++ LINQdotNET/Controller/EmployeeController.cs
@@ -14,5 +14,25 @@ namespace LINQdotNET.Controller
             var employees = employeeService.GetAllEmployees();
             return Ok(employees);
         }
+
+        [HttpGet("/linq/query")]
+        public ActionResult<List<Employee>> GetEmployeesLinqQuery()
+        {
+            var employees = employeeService.GetAllEmployees();
+
+            // LINQ query syntax
+            var query = from employee in employees
+                where employee.Age > 30
+                orderby employee.Name
+                select employee;
+            
+            // query is executed 
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+            return Ok(query);
+        }
     }
 }
