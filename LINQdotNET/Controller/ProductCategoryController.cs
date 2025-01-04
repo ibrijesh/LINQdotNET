0ce500a2d0631fe3861987427f26f9bee1fe25ae
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-01-04 19:28:42 +0530

    4.Display the product names as well as price of all those products which are priced above Rs. 1000 in decreasing order of price

--- LINQdotNET/Controller/ProductCategoryController.cs
+++ LINQdotNET/Controller/ProductCategoryController.cs
@@ -14,12 +14,13 @@ namespace LINQdotNET.Controller
         [HttpGet]
         public ActionResult GetProductCategories()
         {
-            // Display the product names as well as price of all those products which are priced above Rs. 1000
+            // Display the product names as well as price of all those products which are priced above Rs. 1000 in decreasing order of price
 
             var products = productService.GetAllProducts();
 
             var query = from product in products
                 where product.Price > 1000
+                orderby product.Price descending
                 select new { product.ProductName, product.Price };
 
             return Ok(query); // query is executed here, as Ok() serialize the data 
